8e5c6478c91993b544c0a9a5333d98c0835d0bd3
Leonardo <f.leobrito@gmail.com>
2016-11-02 22:49:04 -0300

    Associating Users with Posts and Comments

--- app/assets/javascripts/nav/navCtrl.js
+++ app/assets/javascripts/nav/navCtrl.js
@@ -4,7 +4,7 @@ angular.module('flapperNews')
     'Auth',
     function($scope, Auth){
         $scope.signedIn = Auth.isAuthenticated;
-        $scope.logout =Auth.logout;
+        $scope.logout = Auth.logout;
 
         Auth.currentUser().then(function (user){
             $scope.user = user;
